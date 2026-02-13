fddd1b3eff2ee3060bbbdfa3c47862fda252a62c
JavierMills <javier_bouncing@hotmail.com>
2026-02-12 23:53:15 -0600

    se agrega record v2

--- src/main/java/com/java/lambda/record/ProductDTO.java
+++ src/main/java/com/java/lambda/record/ProductDTO.java
@@ -1,5 +1,18 @@
 package com.java.lambda.record;
 
+import java.util.Objects;
+
 public record ProductDTO(String name, double price) {
+
+    public static String ATRIBUTO = "Valor de atributo estático";
+
+    public ProductDTO {
+        Objects.requireNonNull(name, "El nombre no puede ser nulo");
+    }
    
+    public ProductDTO(String name) {
+        this(name, 0.0);
+    }   
+
+    
 }
